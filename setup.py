1fa7aae336e8ad4f2b9b7de970b4239c20b2b59f
pnowk <pnowk123@gmail.com>
2022-07-24 14:32:41 +0200

    Added attrs and sqlalchemy

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setuptools.setup(
     version='0.0.1',
     description='Clean architecture implementation demo project',
     install_requires=[
-        'Inject==4.3.1'
+        'Inject==4.3.1',
+        'attrs',
+        'sqlalchemy'
     ],
     python_requires=">=3.8",
     extras_require={
